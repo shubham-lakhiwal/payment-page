4d85fbed80330607487548ec76e245c189a72381
Shubham Lakhiwal <shubham.lakhiwal@gmail.com>
2025-10-22 06:23:06 +0530

    Responsiveness for mobile devices

--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "@/components/navigation/NavItem.module.scss";
 import Text from "@/components/text/Text.tsx";
 import type {NavItemProps} from "@/components/navigation/types.ts";
+import {Link} from "react-router";
 
 const NavItem: React.FC<NavItemProps> = (props) => {
   const { label, href, active, icon: Icon } = props;
   return (
     <li className={`${styles.navItem} ${active ? styles.active : ''}`}>
-      <a
-        className={styles.navLink} href={href}
+      <Link
+        className={styles.navLink} to={href}
       >
         <Icon className={styles.navIcon} aria-label={label} />
         <Text size="large" className={`${styles.text}`}>{label}</Text>
-      </a>
+      </Link>
     </li>
   )
 }
